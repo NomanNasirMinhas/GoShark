b4193b3532a4e8b125086d6a82dad5048e1b7dd3
Noman Nasir Minhas <SP17-BSE-010@ISBSTUDENT.COMSATS.EDU.PK>
2024-07-29 11:35:17 +0500

    .

--- frontend/src/routes/Capture.svelte
+++ frontend/src/routes/Capture.svelte
@@ -1,5 +1,6 @@
 <script>
     import logo from "./../assets/images/logo.jpeg";
+    import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
     import { Button } from "flowbite-svelte";
     import { Label, Select } from "flowbite-svelte";
     import {
@@ -138,14 +139,14 @@
   </script>
   
   <main>
-    <img alt="Wails logo" id="logo" src={logo} />
-    <!-- <p style="margin-top: -50px;" class="text-lg dark:text-white">GoShark</p> -->
-    <!-- <div class="result" id="result">{resultText}</div> -->
-    {#if !isAdmin}
-      <div class="result" id="result">
-        You are not an admin. Program will not work properly.
-      </div>
-    {/if}    
+    <Navbar  let:NavContainer color="none">
+        <NavContainer class="border px-5 py-2 rounded-lg bg-white dark:bg-gray-600">
+            <NavBrand href="/">
+              <img src={logo} class="me-3 h-6 sm:h-9" alt="Flowbite Logo" />
+              <!-- <span class="self-center whitespace-nowrap text-xl font-semibold">Flowbite</span> -->
+            </NavBrand>
+            </NavContainer>        
+      </Navbar>
   
     {#if capture_started}
       <div class="packets_div">
